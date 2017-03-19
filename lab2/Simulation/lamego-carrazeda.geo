445846a4ef5581deeeb53a6545fc33e7433fb956
Andre Macedo <Andre Macedo>
2017-03-19 17:54:25 +0000

    new simulations data. python script fixed to use dbm scale

--- lab2/Simulation/lamego-carrazeda.geo
+++ lab2/Simulation/lamego-carrazeda.geo
@@ -1,4 +1,4 @@
-#FILENAME	picture1.bmp
-URL	picture1.bmp
+#FILENAME	lamego-carrazeda.bmp
+URL	lamego-carrazeda.bmp
 TIEPOINT	0	0	-8.717821	 41.40497
 TIEPOINT	1699	599	-7.022179	 40.95503
--- lab2/script.py
+++ lab2/script.py
@@ -4,69 +4,69 @@ import plotly
 import plotly.plotly as py
 import plotly.graph_objs as go
 
-import numpy as np
+path = 'CMO_Lab_02_'
 
 plotly.tools.set_credentials_file(username='MaclighterX8', api_key='0AQhRtD0rdU0xrD2iqPJ')
 
 
 def parse_line(line):
-    split = re.split("\s", line) # split line on whitespaces
-    split = filter(None, split)# remove whitespaces
+    split = re.split("\s", line)  # split line on whitespaces
+    split = filter(None, split)  # remove whitespaces
     values_str = list(split)
     values = []
     for item in values_str:
-        if item!=values_str[-1]: # convert every number (but last one) to float
-            val = item.replace(",",".")
+        if item != values_str[-1]:  # convert every number (but last one) to float
+            val = item.replace(",", ".")
             values.append(float(val))
         else:  # convert last number to int
             values.append(int(item))
     return values
 
-#main
+
+# main
 
 # line filtering regex
-float_re= "[+-]?([0-9]*[,])?[0-9]+"
+float_re = "[+-]?([0-9]*[,])?[0-9]+"
 line_re = " " + float_re + "\s+" + float_re + "\s+" + float_re + "\s+[0-9]+"
-r= re.compile(line_re)
+r = re.compile(line_re)
 
-
-total=0
-counters = [0]*8
-# <0 <10 <20 <30 <40 <50 <60 ...
-#  0  1   2   3   4   5   6  7
+total = 0
+counters = [0] * 8
+# ]..., -120[, [-120, -110[ [-110, -100[... [-70, -60[ [60, ...[
+#  0  1   2   3   4   5   6   7
 
 for line in fileinput.input(input("File?")):
-    if r.match(line) is not None :
+    if r.match(line) is not None:
         total += 1
-        
-        values=parse_line(line)
+
+        values = parse_line(line)
         value = values[2]
-        if value < 0 :
-            counters[0]+=1
-        elif value < 10 :
-            counters[1]+=1
-        elif value < 20 :
-            counters[2]+=1
-        elif value < 30 :
-            counters[3]+=1
-        elif value < 40 :
-            counters[4]+=1
-        elif value < 50 :
-            counters[5]+=1
-        elif value < 60 :
-            counters[6]+=1
+        value = value - 120
+        if -120 > value:
+            counters[0] += 1
+        elif -120 <= value < -110:
+            counters[1] += 1
+        elif -110 <= value < -100:
+            counters[2] += 1
+        elif -100 <= value < -90:
+            counters[3] += 1
+        elif -90 <= value < -80:
+            counters[4] += 1
+        elif -80 <= value < -70:
+            counters[5] += 1
+        elif -70 <= value < -60:
+            counters[6] += 1
         else:
-            counters[7]+=1
+            counters[7] += 1
 
 for index, item in enumerate(counters):
-    print("<{} : {}".format(index*10,item))
+    print("<{} : {}".format(index * 10, item))
 
 print("total : {}".format(total))
 
-
 data = [go.Bar(
-            x=['<0', '<10', '<20', '<30', '<40', '<50', '<60', '<70'],
-            y=counters
-    )]
+    x=['] *, -120[', '[-120, -110[', '[-110, -100[', '[-100, -90[', '[-90, -80[', '[-80, -70[', '[-70, -60[', '[-60, *['],
+    y=counters
+)]
 
-py.iplot(data, filename='CMO Lab2 - ' + fileinput.filename())
+py.iplot(data, filename=path+fileinput.filename())
